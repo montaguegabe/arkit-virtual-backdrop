f2935127760034d267005ce4ba9ae2a508ebafeb
Gabriel Montague <gmontague@college.harvard.edu>
2018-01-01 18:51:24 -0500

    Initial Commit

---
+++ Backdrop Demo/Backdrop_Demo-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Backdrop_Demo-Bridging-Header.h
+//  Backdrop Demo
+//
+//  Created by Gabe Montague on 1/1/18.
+//  Copyright © 2018 Gabe Montague. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
---
+++ Backdrop Demo/ViewController.swift
@@ -0,0 +1,116 @@
+//
+//  ViewController.swift
+//  Backdrop Demo
+//
+//  Created by Gabe Montague on 1/1/18.
+//  Copyright © 2018 Gabe Montague. All rights reserved.
+//
+
+import UIKit
+import Metal
+import MetalKit
+import ARKit
+
+extension MTKView : RenderDestinationProvider {
+}
+
+class ViewController: UIViewController, MTKViewDelegate, ARSessionDelegate {
+    
+    var session: ARSession!
+    var renderer: Renderer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        session = ARSession()
+        session.delegate = self
+        
+        // Set the view to use the default device
+        if let view = self.view as? MTKView {
+            view.device = MTLCreateSystemDefaultDevice()
+            view.backgroundColor = UIColor.clear
+            view.delegate = self
+            
+            guard view.device != nil else {
+                print("Metal is not supported on this device")
+                return
+            }
+            
+            // Configure the renderer to draw to the view
+            renderer = Renderer(session: session, metalDevice: view.device!, renderDestination: view)
+            
+            renderer.drawRectResized(size: view.bounds.size)
+        }
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTap(gestureRecognize:)))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    @objc
+    func handleTap(gestureRecognize: UITapGestureRecognizer) {
+        // Create anchor using the camera's current position
+        if let currentFrame = session.currentFrame {
+            
+            // Create a transform with a translation of 0.2 meters in front of the camera
+            var translation = matrix_identity_float4x4
+            translation.columns.3.z = -0.2
+            let transform = simd_mul(currentFrame.camera.transform, translation)
+            
+            // Add a new anchor to the session
+            let anchor = ARAnchor(transform: transform)
+            session.add(anchor: anchor)
+        }
+    }
+    
+    // MARK: - MTKViewDelegate
+    
+    // Called whenever view changes orientation or layout is changed
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        renderer.drawRectResized(size: size)
+    }
+    
+    // Called whenever the view needs to render
+    func draw(in view: MTKView) {
+        renderer.update()
+    }
+    
+    // MARK: - ARSessionDelegate
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
